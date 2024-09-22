57907c7e3a0afb5d29c8970111a71681ea071dce
Arsh Ilahi <arshilahi74@gmail.com>
2024-09-22 17:54:29 +0530

    a

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,60 @@
+// App.js
+import React, { useState } from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import store from './store';
+import { updateName, updateEmail } from './action';
 
-import React from "react";
-import './../styles/App.css';
+const UserForm = () => {
+  const dispatch = useDispatch();
+  const name = useSelector((state) => state.name);
+  const email = useSelector((state) => state.email);
+  const [inputName, setInputName] = useState('');
+  const [inputEmail, setInputEmail] = useState('');
+
+  const handleNameChange = (e) => {
+    setInputName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setInputEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputName) dispatch(updateName(inputName));
+    if (inputEmail) dispatch(updateEmail(inputEmail));
+    setInputName('');
+    setInputEmail('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={inputName} onChange={handleNameChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={inputEmail} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <h2>Current values in store:</h2>
+      <p>Name - {name}</p>
+      <p>Email - {email}</p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Provider store={store}>
+      <UserForm />
+    </Provider>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/reducer.js
@@ -0,0 +1,20 @@
+// reducer.js
+import { UPDATE_NAME, UPDATE_EMAIL } from './action';
+
+const initialState = {
+  name: '',
+  email: '',
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case UPDATE_NAME:
+      return { ...state, name: action.payload };
+    case UPDATE_EMAIL:
+      return { ...state, email: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
